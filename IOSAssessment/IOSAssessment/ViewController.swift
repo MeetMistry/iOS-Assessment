c7fc3ec609d9c9f3d79e810846cb599a786fd52e
Meet Mistry <meet.m@simformsolutions.com>
2024-05-01 21:41:21 +0530

    Project setup

---
+++ IOSAssessment/IOSAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSAssessment
+//
+//  Created by Meet Mistry on 01/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
