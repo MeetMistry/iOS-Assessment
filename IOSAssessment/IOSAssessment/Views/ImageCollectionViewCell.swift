9ba94fff8da60be089a88d23b2fb385ae8b57162
Meet Mistry <meet.m@simformsolutions.com>
2024-05-02 22:28:07 +0530

    API call

--- IOSAssessment/IOSAssessment/Views/ImageCollectionViewCell.swift
+++ IOSAssessment/IOSAssessment/Views/ImageCollectionViewCell.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let imageCache = NSCache<AnyObject, AnyObject>()
+
 class ImageCollectionViewCell: UICollectionViewCell {
     
     // MARK: -
@@ -17,9 +19,42 @@ class ImageCollectionViewCell: UICollectionViewCell {
     // MARK: -
     // MARK: - Configure cell
     
-    func configureCell(image: UIImage) {
-        imageView.image = image
+    func configureCell(imageUrl: String) {
+        imageView.image = UIImage(named: "placeholder")
+        imageView.loadImage(from: imageUrl as NSString)
         imageView.contentMode = .center
     }
     
 } // End of Class
+
+extension UIImageView {
+    func loadImage(from url: NSString) {
+        
+        if let cachedImage = ImageCache.shared.image(forKey: "\(url)") {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let imageUrl = URL(string: url as String) else { return }
+        
+        URLSession.shared.dataTask(with: imageUrl) { data, response, error in
+            if let error = error {
+                print("Error, \(error.localizedDescription)")
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("Error with response, unexpected status code: \(String(describing: response))")
+                return
+            }
+            
+            if let data = data {
+                if let downloadedImage = UIImage(data: data) {
+                    ImageCache.shared.setImage(downloadedImage, forKey: url)
+                    DispatchQueue.main.async {
+                        self.image = downloadedImage
+                    }
+                }
+            }
+        }.resume()
+    }
+}
